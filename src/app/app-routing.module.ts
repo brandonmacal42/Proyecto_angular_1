53edf0b6f8a34ccf7e3fb3c974507f6157a3534e
Brandon Macal <brandonmacal@ciencias.unam.mx>
2023-10-18 19:23:49 -0600

    Practica 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './modules/product/components/category/category.component';
 const routes: Routes = [
-  { path: 'category', component: CategoryComponent }, 
+  { path: '', component: CategoryComponent }, 
 ];
 
 @NgModule({
--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -4,7 +4,7 @@ import { Category } from '../../_models/category';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./style.css']
+  styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit{
 
@@ -17,8 +17,8 @@ export class CategoryComponent implements OnInit{
   //crea un método llamado getCategories donde le agregues 3 objetos category (con datos dummy) al arreglo categories,
   getCategories() {
     // Agrega 3 objetos de tipo Category con datos dummy al arreglo categories
-    this.categories.push(new Category(1, 'v1', 'Categoria 1', 'Si'));
-    this.categories.push(new Category(2, 'v2', 'Category 2', 'No'));
-    this.categories.push(new Category(3, 'v3', 'Category 3', 'Si'));
+    this.categories.push(new Category(1, '7685', 'Categoria 1', 'En existencia'));
+    this.categories.push(new Category(2, '7639', 'Category 2', 'No hay'));
+    this.categories.push(new Category(3, '7683', 'Category 3', 'En existencia'));
   }
 }
