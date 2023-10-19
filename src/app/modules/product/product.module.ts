6c9db1881092c819d959f1d3966e3d266fd9badd
Brandon Macal <brandonmacal@ciencias.unam.mx>
2023-10-19 10:56:58 -0600

    Practica_7

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './components/category/category.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
@@ -9,7 +11,10 @@ import { CategoryComponent } from './components/category/category.component';
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class ProductModule { }
